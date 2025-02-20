78ebbf54441438b2b6135961340f352218385606
Saad Alangari <sksalangari@gmail.com>
2025-02-20 18:56:32 +0300

    Add files via upload

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,36 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    
+    Parameters:
+    cur (cursor): Cursor to execute PostgreSQL commands in a database session.
+    conn (connection): Connection to the PostgreSQL database.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list.
+    
+    Parameters:
+    cur (cursor): Cursor to execute PostgreSQL commands in a database session.
+    conn (connection): Connection to the PostgreSQL database.
+    """
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Establishes connection to the Redshift cluster, drops all tables, and creates all tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,12 +4,26 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 into staging tables on Redshift.
+    
+    Parameters:
+    cur (cursor): Cursor to execute PostgreSQL commands in a database session.
+    conn (connection): Connection to the PostgreSQL database.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Inserts data from staging tables into the analytics tables on Redshift.
+    
+    Parameters:
+    cur (cursor): Cursor to execute PostgreSQL commands in a database session.
+    conn (connection): Connection to the PostgreSQL database.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -32,6 +46,10 @@ def print_row_counts(cur):
         print(f"{query} → {result[0]} rows")
 
 def main():
+    """Establishes connection to the Redshift cluster, loads data into staging tables,
+    inserts data into analytics tables, and prints row counts for validation.
+    """
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
